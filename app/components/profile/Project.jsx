b9e2cc4f186031495803e807b07fff9e7261feed
Nabih ALI <nabih@buga.fr>
2024-04-04 04:41:27 +0200

    mise en forme done projet done

--- app/components/profile/Project.jsx
+++ app/components/profile/Project.jsx
@@ -26,6 +26,7 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     justifyContent: "space-between",
     paddingHorizontal: "2%",
+    paddingVertical: '1%',
     marginVertical: "1%",
     marginHorizontal: "1%",
   },
--- app/components/profile/Skill.jsx
+++ app/components/profile/Skill.jsx
@@ -1,13 +1,43 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { useEffect, useState } from "react";
+// import * as Progress from 'react-native-progress';
 
 const Skill = ({ name, level }) => {
+  const [levelName, setLevelName] = useState("");
+  const [color, setColor] = useState("black");
+
+  const selectLevelName = () => {
+    switch (true) {
+      case level < 7:
+        setLevelName("Junior");
+        setColor("olive");
+        break;
+      case level < 14:
+        setLevelName("Confirmé");
+        setColor("green");
+        break;
+      case level < 21:
+        setLevelName("Expert");
+        setColor("orange");
+        break;
+      default:
+        setLevelName("Maître");
+        setColor("red");
+        break;
+    }
+  };
+  useEffect(() => {
+    selectLevelName();
+  }, []);
   return (
     <View style={styles.root}>
       <Text style={styles.name} adjustsFontSizeToFit numberOfLines={1}>
         {name}
       </Text>
-      <Text>{level}</Text>
+      <View style={styles.level}>
+        <Text style={{ color: color, fontWeight: 700 }}>{levelName}</Text>
+        <Text style={{ fontWeight: 700 }}>{level}</Text>
+      </View>
     </View>
   );
 };
@@ -19,16 +49,24 @@ const styles = StyleSheet.create({
     width: "auto",
     flex: 1,
     flexDirection: "row",
-    alignItems: 'center',
+    alignItems: "center",
     backgroundColor: "rgba(255,255,255, 1)",
     borderRadius: 5,
     justifyContent: "space-between",
     paddingHorizontal: "2%",
+    paddingVertical: "1%",
     marginVertical: "1%",
     marginHorizontal: "1%",
   },
   name: {
-    fontWeight: "500",
-    width: 250,
+    fontWeight: "700",
+    width: 220,
   },
+  level: {
+    justifyContent: "center",
+    alignItems: "flex-end",
+  },
+  // levelText: {
+  //   color:
+  // }
 });
